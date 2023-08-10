6c8e9c8a2ec37f4e55799a8cf2f5738cef2b1115
Yuval Turejman <yovi.169@gmail.com>
2023-08-10 15:09:43 +0300

    changed db name

--- backend/config/dev.mjs
+++ backend/config/dev.mjs
@@ -1,6 +1,6 @@
 export default  {
   dbURL: 'mongodb+srv://yuvi:yossef169@cluster0.bvwypuk.mongodb.net/?retryWrites=true&w=majority',
-  dbName : 'story_DB'
+  dbName : 'instaneed_DB'
 }
 
 
--- backend/config/prod.mjs
+++ backend/config/prod.mjs
@@ -1,4 +1,4 @@
 export default  {
   dbURL: 'mongodb+srv://yuvi:yossef169@cluster0.bvwypuk.mongodb.net/?retryWrites=true&w=majority',
-  dbName : 'story_DB'
+  dbName : 'instaneed_DB'
 }
